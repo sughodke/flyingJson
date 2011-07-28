32a8c8f53ee04e3d2d5480f2f178b2b007d19fd7
Sid Ghodke <sughodke@ucdavis.edu>
2011-07-28 10:48:26 -0700

    Adding ror server

---
+++ flyingJson-server/flyingjson/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flyingjson::Application.config.secret_token = '5ff268e0e71b6e23e1fd17363b1429f751cb343493c0c35a01c2e28c7cc2636947403a0e4b229c1983a652664acf1624997eb8c146686e8c19c48daf9b7082fe'
